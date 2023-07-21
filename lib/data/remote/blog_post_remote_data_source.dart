e94601e5bd2789df580a2817da4ad16576555a47
alfanalamin <alfanalamin123@gmail.com>
2023-07-21 20:58:51 +0700

    Update berhasil

--- lib/data/remote/blog_post_remote_data_source.dart
+++ lib/data/remote/blog_post_remote_data_source.dart
@@ -1,46 +1,36 @@
-import 'package:flutter_application_1/common/graphql/graphql_util.dart';
+import 'dart:async';
 import 'package:flutter_application_1/common/graphql/query/blogPosts.graphql.dart';
 import 'package:graphql/client.dart';
 import 'package:get_it/get_it.dart';
-import 'dart:async';
+
 abstract class BlogPostRemoteDataSource {
   Future<Query$blogPosts$blogPosts?> getPost();
 }
 
-// class BlogPostRemoteDataSourceImpl implements BlogPostRemoteDataSource {
-//   BlogPostRemoteDataSourceImpl();
-
-//   @override
-// //   Future<Query$blogPosts$blogPosts?> getPost() async {
-// //     final result = await GetIt.I<GraphQLClient>().query$blogPosts(
-// //         Options$Query$blogPosts(fetchPolicy: FetchPolicy.networkOnly));
-// //     checkError(result);
-// //     return result.parsedData?.blogPosts;
-// //   }
-// // }
 class BlogPostRemoteDataSourceImpl implements BlogPostRemoteDataSource {
   BlogPostRemoteDataSourceImpl();
 
+  @override
   Future<Query$blogPosts$blogPosts?> getPost() async {
     final graphQLClient = GetIt.I<GraphQLClient>();
 
-    final completer = Completer<QueryResult>();
     final queryOptions = QueryOptions(
-      document: Options$Query$blogPosts().document, // Use 'document' property
+      document: Options$Query$blogPosts().document,
       fetchPolicy: FetchPolicy.networkOnly,
     );
 
-    graphQLClient.query(queryOptions).then((result) {
-      completer.complete(result);
-    }).catchError((error) {
-      completer.completeError(error);
-    });
-
-    final result = await completer.future;
-    checkError(result);
-
-    // Parse the response data
-    return _parseBlogPostsResponse(result.data);
+    try {
+      final result = await graphQLClient.query(queryOptions);
+      if (result.hasException) {
+        // Handle GraphQL query exceptions
+        throw result.exception!;
+      }
+      return _parseBlogPostsResponse(result.data);
+    } catch (error) {
+      // Handle general exceptions
+      print('Error: $error');
+      return null;
+    }
   }
 
   Query$blogPosts$blogPosts? _parseBlogPostsResponse(
@@ -63,3 +53,16 @@ class BlogPostRemoteDataSourceImpl implements BlogPostRemoteDataSource {
     }
   }
 }
+
+
+// class BlogPostRemoteDataSourceImpl implements BlogPostRemoteDataSource {
+//   BlogPostRemoteDataSourceImpl();
+
+//   @override
+// //   Future<Query$blogPosts$blogPosts?> getPost() async {
+// //     final result = await GetIt.I<GraphQLClient>().query$blogPosts(
+// //         Options$Query$blogPosts(fetchPolicy: FetchPolicy.networkOnly));
+// //     checkError(result);
+// //     return result.parsedData?.blogPosts;
+// //   }
+// // }
--- lib/presentation/page/post_screen.dart
+++ lib/presentation/page/post_screen.dart
@@ -70,10 +70,10 @@
 // }
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/state_enum.dart';
 import 'package:flutter_application_1/presentation/provider/blog_post_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
-
 class BlogPostScreen extends StatefulWidget {
   const BlogPostScreen({super.key});
 
@@ -89,18 +89,7 @@ class _BlogPostScreenState extends State<BlogPostScreen> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(Duration.zero, () {
-        Provider.of<BlogPostProvider>(context, listen: false)
-          ..doGetPost().then((value) {
-            context
-                .read<BlogPostProvider>()
-                .postsData
-                ?.items
-                .forEach((element) {
-              print(element.title);
-            });
-          });
-      });
+      Provider.of<BlogPostProvider>(context, listen: false).doGetPost();
     });
   }
 
@@ -118,28 +107,57 @@ class _BlogPostScreenState extends State<BlogPostScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 100,
-            color: Colors.blue,
-            child: const Center(
-              child: Text(
-                'Blog Post',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Blog Post'),
+    ),
+    body: Consumer<BlogPostProvider>(
+      builder: (context, provider, _) {
+        switch (provider.postState) {
+          case RequestState.Empty:
+            return const Center(
+              child: Text('No data available.'),
+            );
+          case RequestState.Loading:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          case RequestState.Loaded:
+            if (provider.postsData != null) {
+              // Display the fetched data using ListView.builder
+              return SmartRefresher(
+                controller: _refreshController,
+                onRefresh: _onRefresh,
+                onLoading: _onLoading,
+                child: ListView.builder(
+                  itemCount: provider.postsData!.items.length,
+                  itemBuilder: (context, index) {
+                    final post = provider.postsData!.items[index];
+                    return ListTile(
+                      title: Text(post.title ?? ''),
+                      // Add more widgets to display other details of the post
+                    );
+                  },
                 ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+              );
+            } else {
+              // Show an error message if data fetch fails
+              return const Center(
+                child: Text('Failed to fetch data.'),
+              );
+            }
+          case RequestState.Error:
+            return const Center(
+              child: Text('Error fetching data.'),
+            );
+          default:
+            return const Center(
+              child: Text('Unexpected state.'),
+            );
+        }
+      },
+    ),
+  );
+}
 }
